789ae0056dc5df5c3b1ff20a14f945fdbd994df7
starkey <tanishqgupta013@gmail.com>
2025-05-14 16:23:57 +0530

    atoi problem

--- main.java
+++ main.java
@@ -1,69 +1,44 @@
-
 import java.util.*;
+
 class Solution {
-    public int[] plusOne(int[] digits) {
+     public int myAtoi(String s) {
     int num = 0;
-    for(int i = 0 ; i <= digits.length-1 ; i++){
-        num*=10;
-        num += digits[i];
+    int i = 0;
+    s = s.trim();
+
+    if (s.length() == 0) return 0;
+
+    int sign = 1;
+    StringBuilder sb = new StringBuilder(s);
+
+    if (sb.charAt(0) == '-') {
+        sign = -1;
+        i = 1;
+    } else if (sb.charAt(0) == '+') {
+        i = 1;
     }
-    num +=1;
-    String string = String.valueOf(num);
-    int arr[] = new int[string.length()];
-    System.out.println(string);
-    for (int j = arr.length-1 ; j >= 0 ; j--){   
-        arr[j] = num%10;
-        num/=10;
-        System.out.println(arr[j]);
-    }
-    System.out.println(Arrays.toString(arr));
-    return arr;
-}
-}
-class Solution2 {
-    public int[] plusOne(int[] digits) {
-        //1. if last ith digit is less than 9, we add that digit by 1 in-place and return digits array.
-        if(digits[digits.length - 1] < 9){
-            digits[digits.length - 1] += 1;
-            return digits;
-        }
-        //2. declaring an ArrayList so we can insert and append values, when a carry-over occurs, it will be a copy of digits array.
-        ArrayList<Integer> result = new ArrayList<>();
-        for(int digit: digits){
-            result.add(digit);
-        }
-        result.set(digits.length - 1, 0);
-        if(result.size() == 1){
-            result.add(0, 1);
-        }
-        for(int i = digits.length - 2; i >= 0; i --){
-            if(digits[i] == 9){
-                result.set(i, 0);
-                if(i == 0){
-                    result.add(0, 1);
-                }
+
+    for (int j = i; j < sb.length(); j++) {
+        if (sb.charAt(j) >= '0' && sb.charAt(j) <= '9') {
+            int digit = sb.charAt(j) - '0';
+            if (num > (Integer.MAX_VALUE - digit) / 10) {
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
             }
-            else{
-                result.set(i, result.get(i) + 1);
-                break;
-            }
+            num = num * 10 + digit;
+        } else {
+            break;
         }
-        int[] resultToArray = new int[result.size()];
-        for(int i = 0; i < result.size(); i++){
-            resultToArray[i] = result.get(i);
-        }
-        return resultToArray;
     }
+
+    return num * sign;
+}
+
 }
 
 public class main {
     public static void main(String[] args) {
         Solution S = new Solution();
-        Solution2 S2 = new Solution2();
-        int [] Arr = {9,8,7,6,5,4,3,2,1,0};
-        S.plusOne(Arr);//creative solution but worst time complexity
-        S2.plusOne(Arr);//0ms time solution using arrayList
-    }
+        System.out.println(S.myAtoi("-990sdfgs909kja;lfjoafo"));
 
-    
+    }
 }
---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,3 @@
+  int digit = sb.charAt(i)- '0';
+                num*=10;
+                num+=digit;
