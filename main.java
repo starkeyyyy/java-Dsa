df59bff8c75464789ab4c3061b9f08e268d23cdd
starkey <tanishqgupta013@gmail.com>
2025-05-17 18:45:33 +0530

    binary search with first and last last target

--- main.java
+++ main.java
@@ -1,26 +1,42 @@
 import java.util.*;
 
 class Solution {
-    public int strStr(String haystack, String needle) {
-        int index = 0;
-        String result = haystack.replace(needle , "-");
-        for(int i = 0 ; i < haystack.length() ; i++){
-            if(result.contains("-")){
-                if(result.charAt(i) == '-'){
-                index = i;
-                break;
-            }
-            }
-            else{
-                return -1;
+    public int[] searchRange(int[] arr, int target) {
+        int[] result = {-1, -1};        
+        // Find first occurrence
+        int low = 0, high = arr.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] == target) {
+                result[0] = mid;
+                high = mid - 1; // keep searching on left
+            } else if (arr[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
-    return index;  
-    }
-}
+
+        // Find last occurrence
+        low = 0; high = arr.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] == target) {
+                result[1] = mid;
+                low = mid + 1; // keep searching on right
+            } else if (arr[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return result;
+}}
 public class main {
     public static void main(String[] args) {
         Solution S = new Solution();
-        System.out.println(S.strStr("hello", "ll"));
+        int arr[] = {5,7,8,8,8,10};
+        System.out.println(Arrays.toString(S.searchRange(arr , 8)));
     }
 }
